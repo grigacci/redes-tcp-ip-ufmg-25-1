56a8c109a70f8b6f0593ff619e571e441c188141
grigacci <guilhermerigacci@gmail.com>
2025-04-27 17:09:50 -0300

    Arrumando readme

--- trab1/run_clients.sh
+++ trab1/run_clients.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-gnome-terminal --tab --title="Server" -- bash -c "./build/server $3 ; exec bash -i"
-
 for ((i=1; i<=$1; i++))
 do
   gnome-terminal --tab --title="Client[$i]" -- bash -c "./build/client $2 $3; exec bash -i"
